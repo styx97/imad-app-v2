4e9317aebc4a1b9fff6a05191c6bcadddaeb0b92
styx97 <rupaksarkar.cs@gmail.com>
2017-02-07 14:48:54 -0500

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,11 +5,73 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var movies = {
+    title: 'Movies | Rupak Sarkar',
+    heading: 'Feb 7, 2017',
+    content: `<p>
+                   These are the contents for my movie page.These are the contents for my movie page.These are the contents for my movie page.
+                   These are the contents for my movie page.These are the contents for my movie page.These are the contents for my movie page.
+                   These are the contents for my movie page.These are the contents for my movie page.These are the contents for my movie page.
+               </p>
+               <p>
+                   These are the contents for my movie page.These are the contents for my movie page.These are the contents for my movie page.
+                   These are the contents for my movie page.These are the contents for my movie page.These are the contents for my movie page.
+                   These are the contents for my movie page.These are the contents for my movie page.These are the contents for my movie page.
+               </p>
+               <p>
+                   These are the contents for my movie page.These are the contents for my movie page.These are the contents for my movie page.
+                   These are the contents for my movie page.These are the contents for my movie page.These are the contents for my movie page.
+                   These are the contents for my movie page.These are the contents for my movie page.These are the contents for my movie page.
+               </p>`
+    
+};
+
+function createTemplate(data)  { 
+    var title = data.title;
+    var date = data.date;
+    var heading = data.heading;
+    var content = data.content;
+    
+    var htmlTemplate =
+    `<html>
+      <head>
+          <title>
+             ${title}
+          </title>
+           <meta name = "viewport" content = "width = device-width, initial-scale=1" />  
+           <link href="/ui/style.css" rel="stylesheet" />
+         
+      </head>
+       <body>
+           <div class = "container">
+               <div>
+                   <a href = "/">Home</a>>
+               </div>
+               <hr/>
+               <h3>
+                   ${heading} 
+               </h3>
+               <div>
+                   ${date}
+                   
+               </div>
+               <div>
+                   ${content}
+                   </p>
+               </div>
+           </div>        
+       </body>   
+    </html>
+    `;
+    return  htmlTemplate;
+}
+
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/movies',function(req,res) {
-    res.sendFile(path.join(__dirname, 'ui', 'movies.html'));
+    res.send(createTemplate(movies));
 });
 
 app.get('/music',function(req,res) {
