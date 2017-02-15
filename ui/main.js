09b59bcbe9fa7937a42d5568bd9e4a7bc3e17c3d
styx97 <rupaksarkar.cs@gmail.com>
2017-02-15 21:37:30 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,3 +4,11 @@ console.log('Loaded!');
 var element = document.getElementById('main-text');
 
 element.innerHTML = "New Value"; 
+
+//Move the iamge
+var img =  document.getElementById('madi');
+
+
+img.onclick = function() {
+    img.style.marginLeft = '100px';
+};
