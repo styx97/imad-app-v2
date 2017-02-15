304985e2149685fab76980d0f9e935160eca906a
styx97 <rupaksarkar.cs@gmail.com>
2017-02-15 21:54:42 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -10,12 +10,12 @@ var img =  document.getElementById('madi');
 
 var marginLeft = 0;
 function moveRight() {
-    marginLeft = marginLeft + 5;
+    marginLeft = marginLeft + 10;
     img.style.marginLeft = marginLeft + 'px';
 }
 
 
 img.onclick = function() {
-    var interval = setInterval(moveRight,10);
+    var interval = setInterval(moveRight,50);
    
 };
