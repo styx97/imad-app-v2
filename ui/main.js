a3315a77df79c3c33d1ef672765d6c1453e61533
styx97 <rupaksarkar.cs@gmail.com>
2017-02-15 21:46:52 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -11,6 +11,5 @@ var img =  document.getElementById('madi');
 
 img.onclick = function() {
     img.style.marginLeft = '100px';
-    img.style.marginRight= '200px';
-    img.style.marginLeft = '100px';
+   
 };
