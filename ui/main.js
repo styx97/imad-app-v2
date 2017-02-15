250207a5a47e2fb1734a78606f5b6b6fb1c6d265
styx97 <rupaksarkar.cs@gmail.com>
2017-02-15 21:55:58 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,7 +3,7 @@ console.log('Loaded!');
 //Change the text of the main- text div
 `var element = document.getElementById('main-text');
 element.innerHTML = "New Value"; 
-`
+`;
 //Move the iamge
 var img =  document.getElementById('madi');
 
