6131a368dc1a2ee37c8805adfdc10d8781b106ff
styx97 <rupaksarkar.cs@gmail.com>
2017-02-16 22:54:43 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -86,7 +86,7 @@ function createTemplate(data)  {
 
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'rupak.html'));
+  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 
