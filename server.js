66794c0219ef553fa8f71be1428ce059d14de139
styx97 <rupaksarkar.cs@gmail.com>
2017-02-25 22:30:55 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,15 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var counter = 0;
+app.get("/counter", function(req,res){
+   counter = counter + 1;
+   res.send(counter.toString());
+   
+});
+
+
+
 
 var articles = { 
     'movies' : {
