b4539f14c321e4f34db9ad1f8ddc9b94c835b16b
styx97 <rupaksarkar.cs@gmail.com>
2017-02-26 00:27:45 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,23 +1,26 @@
-console.log('Loaded!');
+//counter code
+var button = document.getElementbyId ('counter');
 
-/*
-//Change the text of the main- text div
-`var element = document.getElementById('main-text');
-element.innerHTML = "New Value"; 
-`;
-//Move the iamge
-
-var img =  document.getElementById('madi');
-
-var marginLeft = 0;
-function moveRight() {
-    marginLeft = marginLeft + 10;
-    img.style.marginLeft = marginLeft + 'px';
-}
-
-
-img.onclick = function() {
-    var interval = setInterval(moveRight,50);
-   
+button.onclick = function () {
+  
+  //create an object
+  var request = new XMLHttpRequest();
+  
+  //Capture the response and store it in a variable 
+  request.onreadystatechange = function () {
+      if (request.readyState === XMLHttpRequest.DONE) {
+          //take some action
+          if (request.status ===  200) {
+              var counter = request.responseText;
+              var span = document.getElementById('count');
+              span.innerHTML = counter.toString();
+    
+          }
+      }
+  //not done yet
+      
+  };
+  //Make the request
+  request.open('GET' , 'http://styx97.imad.hasura-app.io/counter', true);
+  request.send(null);
 };
-*/
