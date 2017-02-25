807bb6e92f6470ba41b1cdd6ad2958de72d01f4f
styx97 <rupaksarkar.cs@gmail.com>
2017-02-26 01:03:07 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -13,7 +13,7 @@ button.onclick = function() {
           if (request.status ===  200) {
               var counter = request.responseText;
               var span = document.getElementById("count");
-              span.innerHTML = counter.getString();
+              span.innerHTML = counter;
     
           }
       }
