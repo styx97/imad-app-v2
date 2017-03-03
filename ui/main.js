3d675a0aa4c988f5c9295e49d42de91e4affcf73
styx97 <rupaksarkar.cs@gmail.com>
2017-03-04 00:30:09 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -32,18 +32,37 @@ var name = nameInput.value;
 var submit = document.getElementById('submit_btn');
 submit.onclick = function() {
     // make a request to the server and send the name
+    //create a request object
+  var request = new XMLHttpRequest();
+  
+  //Capture the response and store it in a variable 
+  request.onreadystatechange = function () {
+      if (request.readyState === XMLHttpRequest.DONE) {
+          //take some action
+          if (request.status ===  200) {
+             // Capture a lidt of names and render it as a list
+            var names = request.responseText;
+            names = JSON.parse(names);
+            var list = '';
+            for (var i = 0; i<names.length; i++ ) {
+              list += '<li>'+ names[i] + '</li>';   
+            }
+            var ul = document.getElementById('namelist');
+            ul.innerHTML = list;
+     
     
-    
-    // Capture a lidt of names and render it as a list
-    var names = ['name1','name2','name3','name4'];
-    var list = '';
-    for (var i = 0; i<names.length; i++ ) {
-      list += '<li>'+ names[i] + '</li>';   
-    }
-    var ul = document.getElementById('namelist');
-    ul.innerHTML = list;
-    
+          }
+      }
+  //not done yet
+      
+  };
+  //Make the request
+  request.open('GET' , 'http://styx97.imad.hasura-app.io/submit-name?name=' + name, true);
+  request.send(null);
     
 };
+    
+    
+
 
 
